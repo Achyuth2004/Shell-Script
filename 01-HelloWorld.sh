5ddc48507a7747be52e59e8635d29c677bfdb5f4
Achyuth2004 <achyuthkayala2004@gmail.com>
2026-06-25 14:26:37 +0530

    basic script

---
+++ 01-HelloWorld.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This is a simple shell script that prints "Hello, World!" to the terminal.
+echo "Hello, World!"
+echo "This is a basic shell script example."
+echo "You can run this script by saving it to a file and executing it in the terminal."
