97cc733302f96b6586f0cb74d883f293a9a77d88
Achyuth2004 <achyuthkayala2004@gmail.com>
2026-06-30 00:11:46 +0530

    loops

---
+++ 07-Loops.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+user= $(id -u)
+if[ $user -eq 0]; then
+    echo "You are root user,No access🤪!"
+    exit 1
+else
+    echo "You are common user!!!!...."
+fi
+
+for i in $@:
+do
+    echo "Package to install --> $i"
+done
